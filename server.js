af08eb4aa36cba634e10bdbc5389e949ff540bf6
Yevgen Kruglyk <eugen.kr@gmail.com>
2017-08-09 22:10:11 +0300

    added projects

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -52,6 +52,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     caption: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    caption: 'Projects Page'
   });
 });
 
